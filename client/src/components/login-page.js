4a62789f0940f29159ea2bd288701d7f1170d18d
brianmsj <brianmsj@gmail.com>
2017-06-18 15:39:53 -0700

    made login a stateful component

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as Cookies from 'js-cookie';
 
-export default function LoginPage() {
-    return <a href={'api/auth/google'}>Login with Google</a>;
+
+export class LoginPage extends Component {
+
+    render() {
+        return (
+            <a href={'/api/auth/google'}>Login with Google</a>
+        );
+    }
 }
+
+export default LoginPage;
