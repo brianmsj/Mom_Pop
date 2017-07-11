dd541cea215d237f7c555e4c485317ae3d5f2b95
brianmsj <brianmsj@gmail.com>
2017-07-11 08:18:24 -0700

    added value onChange and onSubmit to main search form

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -5,6 +5,22 @@ import React, { Component } from 'react';
 
 
 export class Search extends Component {
+    constructor()  {
+      super();
+      this.state= {
+        zipCode: "",
+        searchTerm:""
+      }
+      this.searchTerm = this.searchTerm.bind(this)
+      this.zipCode = this.zipCode.bind(this)
+    }
+    zipCode(event) {
+      this.setState({zipCode: event.target.value
+      })
+    }
+    searchTerm(event) {
+      this.setState({searchTerm: event.target.value})
+    }
 
     render() {
         return (
@@ -14,8 +30,8 @@ export class Search extends Component {
                 <p className='search-titles one-title'>Hire Local, Work Local</p>
                 <p className='search-titles'>Helping local small businesses find great employees</p>
                 <div>
-                <input className='main-search-input'type='text' placeholder='Search Jobs'></input>
-                <input className='main-search-input'type='text' placeholder='Enter Location'></input>
+                <input className='main-search-input' onChange={this.searchTerm}type='text' placeholder='Search Jobs'></input>
+                <input className='main-search-input' onChange={this.zipCode}type='text' placeholder='Enter Location'></input>
                 </div>
                 <button className='main-search-button' type='submit'>SEARCH</button>
               </div>
